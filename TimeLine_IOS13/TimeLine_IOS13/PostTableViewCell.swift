1536a7ea964fe5d9e4b150d6a12698296457664f
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-12 14:23:56 -0600

    Create/Impelement CustomCell

---
+++ TimeLine_IOS13/TimeLine_IOS13/PostTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostTableViewCell.swift
+//  TimeLine_IOS13
+//
+//  Created by Bradley GIlmore on 6/12/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    var post: Post? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        guard let post = post else { return }
+        
+        self.imageView?.image = post.photo
+    }
+    
+}
