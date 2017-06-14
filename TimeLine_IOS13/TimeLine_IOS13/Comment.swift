345e7d3e20cab07323366c4d77d56e2cc02301b1
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-14 11:50:15 -0600

    Update Comment for CloudKitSync Functionality

--- TimeLine_IOS13/TimeLine_IOS13/Comment.swift
+++ TimeLine_IOS13/TimeLine_IOS13/Comment.swift
@@ -7,23 +7,45 @@
 //
 
 import Foundation
+import CloudKit
 
-class Comment {
+class Comment: CloudKitSyncable {
+    
+    //MAKR: - Keys
+    
+    static let kType = "Comment"
+    static let kText = "text"
+    static let kPost = "post"
+    static let kTimestamp = "timestamp"
     
     //MARK: - Internal Properties
     
     let text: String
     let timestamp: Date
-    let post: Post
+    let post: Post?
     
     //MARK: - Initializers
     
-    init(text: String, timestamp: Date = Date(), post: Post) {
+    init(text: String, timestamp: Date = Date(), post: Post?) {
         
         self.text = text
         self.timestamp = timestamp
         self.post = post
     }
+    
+    // MARK: CloudKitSyncable
+    
+    convenience required init?(record: CKRecord) {
+        
+        guard let timestamp = record.creationDate,
+            let text = record[Comment.kText] as? String else { return nil }
+        
+        self.init(text: text, timestamp: timestamp, post: nil)
+        cloudKitRecordID = record.recordID
+    }
+    
+    var cloudKitRecordID: CKRecordID?
+    var recordType: String { return Comment.kType }
     
 }
 
@@ -32,6 +54,20 @@ class Comment {
 extension Comment: SearchableRecord {
     func matches(searchTerm: String) -> Bool {
         return text.contains(searchTerm)
+    }
+}
+
+extension CKRecord {
+    convenience init(_ comment: Comment) {
+        guard let post = comment.post else { fatalError("Comment's Post is nil") }
+        let postRecordID = post.cloudKitRecordID ?? CKRecord(post).recordID
+        let recordID = CKRecordID(recordName: UUID().uuidString)
+        
+        self.init(recordType: comment.recordType, recordID: recordID)
+        
+        self[Comment.kTimestamp] = comment.timestamp as CKRecordValue?
+        self[Comment.kText] = comment.text as CKRecordValue?
+        self[Comment.kPost] = CKReference(recordID: postRecordID, action: .deleteSelf)
     }
 }
 
