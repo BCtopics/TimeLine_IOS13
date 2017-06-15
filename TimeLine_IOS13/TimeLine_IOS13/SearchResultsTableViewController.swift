f945252f00ede667f0e90aefab686a2984e47920
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-15 11:31:08 -0600

    Bug Fix on View Sizing

--- TimeLine_IOS13/TimeLine_IOS13/SearchResultsTableViewController.swift
+++ TimeLine_IOS13/TimeLine_IOS13/SearchResultsTableViewController.swift
@@ -26,8 +26,9 @@ class SearchResultsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "searchPostCell", for: indexPath) as? PostTableViewCell else { return UITableViewCell() }
-
-        guard let post = resultsArray[indexPath.row] as? Post else { return UITableViewCell() }
+        
+       guard let post = resultsArray[indexPath.row] as? Post else { return UITableViewCell() }
+//        let post = PostController.shared.posts[indexPath.row]
         cell.post = post
 
         return cell
